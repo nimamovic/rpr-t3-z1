11d89f27bd721871ae926c09fd6ee9c48edf75bc
nimamovic <nimamovic3@etf.unsa.ba>
2018-11-01 10:11:08 +0100

    dodana metoda hashCode

--- src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -1,10 +1,15 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
-public abstract class TelefonskiBroj {
+public abstract class TelefonskiBroj implements Comparable<TelefonskiBroj> {
 
 	public abstract String ispisi();
 
 	@Override
 	public abstract int hashCode();
+
+	@Override
+	public int compareTo(TelefonskiBroj t){
+		return this.ispisi().compareTo(t.ispisi());
+	}
 
 }
