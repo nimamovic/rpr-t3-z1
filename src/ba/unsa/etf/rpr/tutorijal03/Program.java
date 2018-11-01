96439ab6b6ccbeac8d6141d551964f40487583e4
nimamovic <nimamovic3@etf.unsa.ba>
2018-11-01 10:18:09 +0100

    nedovrsena klasa program

---
+++ src/ba/unsa/etf/rpr/tutorijal03/Program.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+
+public class Program {
+
+    public static void main(String[] args) {
+	    /*ako neko unese Sarajevo onda Grad.ValueOf(grad)
+	    ako pozivni broj Grad g= izPozivnog (s)
+	     */
+    }
+}
